6434febe55db07caeb1a6a4b5b1b4644d678d1c3
Santiago Chimko <santiagochimko@gmail.com>
2023-09-26 14:17:52 -0300

    css changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./app.css";
 import CreditCardUi from "./components/CreditCardUi";
 function App() {
   return (
-    <div className="container mt-5">
+    <div className="container rounded mt-5 p-5 w-25 bg-light">
       <h2 className="mb-4">React Credit Card Payment UI Componenet Example</h2>
       <CreditCardUi />
     </div>
